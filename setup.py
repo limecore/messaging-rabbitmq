0ba3b5adc67a93ebff63f53365fa537af6072eed
Daniel Bradberry <daniel@danielbradberry.com>
2020-04-14 21:19:57 +0100

    Initial Import: extracted RabbitMQ Messaging from source project

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="limecore-messaging-rabbitmq",
+    version="0.0.1",
+
+    description="limecore: Messaging (RabbitMQ)",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/limecore/messaging-rabbitmq",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    author="Daniel Bradberry",
+    author_email="daniel@danielbradberry.com",
+    
+    license='MIT',
+    
+    package_dir={'': 'src'},
+    packages=[
+        'limecore.messaging.rabbitmq',],
+    
+    install_requires=required,
+    python_requires='>=3.7',
+)
